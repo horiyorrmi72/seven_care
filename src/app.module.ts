da0323c08009e6984dfa22cbd4754e8d599f8e62
yusuf <horiyorrmi72@gmail.com>
2024-10-09 17:19:55 +0100

    app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
-  }), MongooseModule.forRoot('mongodb://127.0.0.1:27017'), UserModule, ],
+  }), MongooseModule.forRoot('mongodb://127.0.0.1:27017'), UserModule,AuthModule ],
   controllers: [AppController, ],
   providers: [AppService],
 })
