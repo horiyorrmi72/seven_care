0776ff10d887c830db59210620bbc7de51189fcb
yusuf <horiyorrmi72@gmail.com>
2024-10-09 17:20:25 +0100

    auth DTO

---
+++ src/DTO/auth.dto.ts
@@ -0,0 +1,17 @@
+export class RegisterDto {
+    readonly name: string;
+    readonly email: string;
+    readonly username: string;
+    readonly address: string;
+    readonly password: string;
+    readonly DOB: string;
+    readonly allergies: string[];
+    readonly current_medications: string[];
+    readonly life_style: string;
+    readonly emergency_contact: string;
+}
+
+export class LoginDto {
+    readonly email: string;
+    readonly password: string;
+}
