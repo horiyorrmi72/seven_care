0737e42b3f431c8346c1acf9738d52553c6f153a
yusuf <horiyorrmi72@gmail.com>
2024-10-14 13:46:36 +0100

    feat(jwtModule): added jwt module to use the module globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
+import { DoctorModule } from './doctor/doctor.module';
 
 
 
@@ -13,7 +14,7 @@ import { JwtModule } from '@nestjs/jwt';
   imports: [ConfigModule.forRoot({
     isGlobal: true,
   }), MongooseModule.forRoot('mongodb://127.0.0.1:27017/seven_care'), JwtModule.register({ global: true, secret: process.env.JWT_SECRET, signOptions: { expiresIn: '5400s' } }),
-    UserModule, AuthModule, ],
+    UserModule, AuthModule, DoctorModule, ],
   controllers: [AppController,],
   providers: [AppService],
 })
