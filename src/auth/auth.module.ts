8de2e75bd8e41751f656fd605b5900281801f3e8
yusuf <horiyorrmi72@gmail.com>
2024-10-14 13:42:13 +0100

    feat(doctors module for login): added doctors to the module to allow doctors login via the same login endpoint

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { User, UserSchema } from 'src/schemas/users.schema';
+import { Doctor,DoctorSchema } from 'src/schemas/doctors.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthService } from './auth.service';
 
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema },{name:Doctor.name, schema:DoctorSchema}]), 
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService,]
 })
 export class AuthModule { }
