34b7024a980d43da64a19a11fa3bdff1a5762967
yusuf <horiyorrmi72@gmail.com>
2024-10-26 08:54:06 +0100

    fix(appointments data): fixed the appointment to gather all appointment details

--- src/schemas/users.schema.ts
+++ src/schemas/users.schema.ts
@@ -1,10 +1,11 @@
 import { Prop, raw, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { HydratedDocument } from "mongoose";
+import { Appointments } from "./appointments.schema";
+import mongoose, { HydratedDocument, Types } from "mongoose";
 import { timestamp } from "rxjs";
 
 export type UserDocument = HydratedDocument<User>;
 
-@Schema({timestamps:true})
+@Schema({ timestamps: true })
 export class User {
     @Prop({ required: true, trim: true })
     name: string;
@@ -15,20 +16,24 @@ export class User {
     @Prop({ required: true, trim: true })
     username: string;
 
-    @Prop({ required: true, minlength: 6 , select:false})
+    @Prop({ required: true, minlength: 6, select: false })
     password: string;
 
     @Prop({ trim: true })
     address: string;
 
+    @Prop({ type: String })
+    profilePicture: string;
+
     @Prop({ type: Date })
     DOB: Date;
+
     @Prop({ trim: true })
     phoneNumber: string;
 
-    @Prop({ trim: true, enum: ['user','admin'],default: 'user' })
+    @Prop({ trim: true, enum: ['user', 'admin'], default: 'user' })
     role: string;
-    
+
 
     @Prop(raw({
         allergies: { type: [String] },
@@ -43,6 +48,21 @@ export class User {
         emergency_contact: string
 
     }
+
+    @Prop([{ type: mongoose.Schema.Types.ObjectId, ref: 'Appointments' }])
+    appointments: Types.ObjectId[];
+
+    // @Prop([raw({
+    //     appointmentDate: { type: Date, required: true },
+    //     appointmentDetails: { type: mongoose.Types.ObjectId, ref: 'Appointments' },
+    //     doctor: { type: mongoose.Types.ObjectId, ref: 'Doctor' }
+    // })])
+    // // appointments:mongoose.Schema.Types.ObjectId[]
+    // appointments: {
+    //     appointmentDate: Date;
+    //     appointmentDetails: mongoose.Types.ObjectId;
+    //     doctorId: mongoose.Types.ObjectId;
+    // }[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
