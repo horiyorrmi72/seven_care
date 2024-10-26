8af612154c0b4b2605e04bebb9bc1064cea41a45
yusuf <horiyorrmi72@gmail.com>
2024-10-26 09:12:20 +0100

    feat(user controller):added user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Patch, Post, Put, Req } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Param, Patch, Post, Put, Req } from '@nestjs/common';
 import { User } from 'src/schemas/users.schema';
 import { UserService } from './user.service';
 import { UpdateUserDto } from 'src/DTO/user.dto';
@@ -11,9 +11,15 @@ export class UserController {
         return this.userService.getUsers();
     }
 
+    @Get('profile/:id')
+    getUserProfile(@Param('id') id: string) {
+        if (!id) { throw new BadRequestException('No user with the provided data found!') }
+        return this.userService.getUserProfile(id);
+    }
+
     @Patch('profile/:id')
-    updateProfile(@Param('id') id:string, @Body() updateUserDto:UpdateUserDto) {
-        return this.userService.updateUserDetails(id,updateUserDto);
+    updateProfile(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+        return this.userService.updateUserDetails(id, updateUserDto);
     }
 
 }
