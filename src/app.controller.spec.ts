b23cc6ca74e847ef2a8cede146c5c5b51d741ece
yusuf <horiyorrmi72@gmail.com>
2024-10-26 09:04:42 +0100

    refactor(update test case): changed test for the / endpoint

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Welcome to Seven Care MediCare!"', () => {
+      expect(appController.getHello()).toBe('Welcome to Seven Care MediCare!');
     });
   });
 });
