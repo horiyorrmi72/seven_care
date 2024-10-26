edd960dde2fca60b4d42a1d263af2dc87ec543c0
yusuf <horiyorrmi72@gmail.com>
2024-10-26 09:15:44 +0100

    refactor(appointment model): added appointment model to user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { User, UserSchema } from 'src/schemas/users.schema';
+import { Appointments, AppointmentSchema } from 'src/schemas/appointments.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }, { name: Appointments.name, schema: AppointmentSchema }])],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService,]
 })
 export class UserModule { }
