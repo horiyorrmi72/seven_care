096070b08901b304ad820a95a2fde13107b2ab8b
yusuf <horiyorrmi72@gmail.com>
2024-10-28 16:17:44 +0100

    refactor(existing Doctor check):this will check if doctor with similar data exist before creating a new doctor profile

--- src/doctor/doctor.service.ts
+++ src/doctor/doctor.service.ts
@@ -1,8 +1,8 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { Doctor } from 'src/schemas/doctors.schema';
 import { DoctorDTO, updateDoctorsDataDto } from 'src/DTO/doctor.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
 
 
@@ -11,15 +11,90 @@ export class DoctorService {
     constructor(@InjectModel(Doctor.name) private doctorModel: Model<Doctor>) { }
 
 
-    async getAllDoctors(): Promise<Doctor[]>{
-        const result = await this.doctorModel.find().exec();
+    async getAllDoctors(): Promise<{
+        total: number; doctors: Doctor[]
+    }> {
+        const totalDoctors = await this.doctorModel.countDocuments();
+        const result = await this.doctorModel.find().select({ 'role': false, 'password': false }).exec();
         if (result.length === 0) { throw new NotFoundException('No Doctor found') }
-        
-        return result;
+        return {
+            total: totalDoctors,
+            doctors: result
+        }
+
 
     }
 
+    async getDoctorById(id: string): Promise<Doctor> {
+        const doctor = await this.doctorModel.aggregate([
+            { $match: { _id: new mongoose.Types.ObjectId(id) } },
+            {
+                $lookup: {
+                    from: 'appointments',
+                    localField: '_id',
+                    foreignField: 'doctorId',
+                    as: 'appointments',
+
+                }
+            },
+            {
+                $lookup: {
+                    from: 'appointments',
+                    localField: '_id',
+                    foreignField: 'patientId',
+                    as: 'patients',
+                }
+            },
+            {
+                $project: {
+                    _id: 1,
+                    name: 1,
+                    email: 1,
+                    designation: 1,
+                    department: 1,
+                    appointments: {
+                        $map: {
+                            input: '$appointments',
+                            as: 'appointment',
+                            in: {
+                                _id: '$$appointment._id',
+                                status: '$$appointment.status',
+                                appointmentDate: '$$appointment.appointmentDate',
+                                patientDetails: {
+                                    $arrayElemAt: [
+                                        {
+                                            $filter: {
+                                                input: '$patients',
+                                                as: 'patient',
+                                                cond: {
+                                                    $eq: ['$$patient._id',
+                                                        '$$appointment.patientId'
+                                                    ]
+                                                }
+                                            }
+                                        },
+                                        0
+                                    ]
+                                }
+                            }
+                        }
+                    }
+
+
+                }
+            }
+
+        ])
+
+        if (!doctor) { throw new BadRequestException('No doctor with the provided data found.') }
+        return doctor[0];
+    }
+
     async createDoctor(doctorDto: DoctorDTO): Promise<Doctor> {
+        const existingDoctor = await this.doctorModel.findOne({ email: doctorDto.email }).exec();
+        if (existingDoctor) {
+            throw new ConflictException('Doctor with similar details already exists');
+        }
         const { password } = doctorDto;
         if (!password) {
             throw new BadRequestException('Password is required');
