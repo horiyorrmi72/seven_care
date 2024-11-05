bf3e27449299199f37e91dbad5ea290ecb036742
yusuf <horiyorrmi72@gmail.com>
2024-11-05 17:12:21 +0100

    refactor(api prefix):this commit will add prefix of 'api' to all api endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { JwtAuthGuard } from './utils/guards/jwt-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
   await app.listen(3000);
 }
 bootstrap();
