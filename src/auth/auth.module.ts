4e2da71a984d1f0c7cb1264f44a264a466cb5af7
yusuf <horiyorrmi72@gmail.com>
2024-11-05 17:07:06 +0100

    feat(passport-jwt):this commit will add passport jwt for handling user authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,12 +4,19 @@ import { User, UserSchema } from 'src/schemas/users.schema';
 import { Doctor,DoctorSchema } from 'src/schemas/doctors.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthService } from './auth.service';
+import { jwtStrategy } from 'src/utils/passport/passport.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema },{name:Doctor.name, schema:DoctorSchema}]), 
+  imports: [ConfigModule.forRoot(),MongooseModule.forFeature([{ name: User.name, schema: UserSchema }, { name: Doctor.name, schema: DoctorSchema }]), PassportModule, JwtModule.register({
+    secret: process.env.JWT_SECRET,
+    signOptions:{expiresIn:'15m'}
+  }),
   ],
   controllers: [AuthController],
-  providers: [AuthService,]
+  providers: [AuthService,jwtStrategy]
 })
 export class AuthModule { }
