7a22e16341dc725ba1886ce9f94b00e8e0ce4b99
yusuf <horiyorrmi72@gmail.com>
2024-11-17 00:20:56 +0100

    feat(config tailwind):tailwind config file

---
+++ frontend/seven_care/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    colors: {
+      medical_color: '#0027BF',
+      form_background_color: '#EBEBEB',
+      greetings_text_color: '#0077E9',
+      main_background: '#FFFFFF',
+      dashboard_side_nav_background: '#F3F9FF',
+      dashboard_side_nav_text_color: '#878787',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
